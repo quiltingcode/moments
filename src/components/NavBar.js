3b8f1761d5dc301407cf8516404b581cfb098a75
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-29 08:13:30 +0000

    Connect css styles to navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="text-left ms-auto">
             <Nav.Link>
               <i className="fas fa-home"></i>Home
             </Nav.Link>

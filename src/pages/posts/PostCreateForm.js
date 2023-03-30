78a0ad10c5bd5c7b6850e2a3f9136f05670c4b41
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-30 15:45:16 +0000

    Add post form

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -5,24 +5,65 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 import Upload from "../../assets/upload.png";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
 
 function PostCreateForm() {
 
   const [errors, setErrors] = useState({});
+  const [postData, setPostData] = useState({
+    title: '',
+    content: '',
+    image: '',
+  });
 
+  const { title, content, image } = postData
+
+  const handleChange = (event) => {
+    URL.revokeObjectURL(image);
+    setPostData({
+        ...postData,
+        [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length){
+        setPostData({
+            ...postData,
+            image: URL.createObjectURL(event.target.files[0])
+        })
+    }
+  }
 
   const textFields = (
     <div className="text-center">
-      {/* Add your form fields here */}
+        <Form.Group>
+            <Form.Label>Title</Form.Label>
+            <Form.Control 
+                type="text" 
+                name="title" 
+                value={title}
+                onChange={handleChange}
+            />
+        </Form.Group>
 
-    
-    
+        <Form.Group>
+            <Form.Label>Content</Form.Label>
+            <Form.Control 
+                as="textarea" 
+                rows={6} 
+                name="content" 
+                value={content}
+                onChange={handleChange}
+            />
+        </Form.Group>
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
         onClick={() => {}}
@@ -43,14 +84,39 @@ function PostCreateForm() {
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              
+              {image ? (
+                <>
+                    <figure>
+                        <Image className={appStyles.Image} src={image} rounded />
+                    </figure>
+                    <div>
+                        <Form.Label 
+                            className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                            htmlFor="image-upload"
+                        >
+                            Change the Image
+                        </Form.Label>
+                    </div>
+                </>
+              ) : (
                 <Form.Label
-                  className="d-flex justify-content-center"
-                  htmlFor="image-upload"
-                >
-                  ASSET
-                </Form.Label>
+                className="d-flex justify-content-center"
+                htmlFor="image-upload"
+              >
+                <Asset 
+                  src={Upload} 
+                  message="Click or tap to upload an image" 
+                />
+              </Form.Label>
+              )}
 
+                <Form.File
+                    id="image-upload"
+                    accept="image/*"
+                    onChange={handleChangeImage}
+                >
+
+                </Form.File>
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
           </Container>

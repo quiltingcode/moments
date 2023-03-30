351e44ea36fcbee8ee28797ce3d3864efb68a544
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-30 10:19:05 +0000

    Add axios access interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,5 @@ axios.defaults.baseURL = 'https://django-rest-kelz.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();

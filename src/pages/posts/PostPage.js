ccb7811e8825a9a3364c0a28f1f1df212a9b57e0
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-05 14:12:00 +0000

    Add delete comment function

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,13 +5,13 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import Comment from "../comments/Comment";
 
 function PostPage() {
   const { id } = useParams();
@@ -21,22 +21,22 @@ function PostPage() {
   const profile_image = currentUser?.profile_image;
   const [comments, setComments] = useState({ results: [] });
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try{
-                const [{data: post}, { data: comments }] = await Promise.all([
-                    axiosReq.get(`/posts/${id}`),
-                    axiosReq.get(`/comments/?post=${id}`)
-                ])
-                setPost({results: [post]})
-                setComments(comments);
-            } catch(err){
-                console.log(err)
-            }
-        };
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }, { data: comments }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
+        setPost({ results: [post] });
+        setComments(comments);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-        handleMount();
-    }, [id]);
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
@@ -56,13 +56,18 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map(comment => (
-              <Comment key ={comment.id} {...comment} />
+            comments.results.map((comment) => (
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+              />
             ))
           ) : currentUser ? (
-            <span>No comments yet, be the first to comment!</span> 
+            <span>No comments yet, be the first to comment!</span>
           ) : (
-            <span>No comments...yet</span>
+            <span>No comments... yet</span>
           )}
         </Container>
       </Col>

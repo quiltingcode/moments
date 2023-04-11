03ecbcce3d20046c7a9854bb5a684f7c0df9bffb
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-11 09:34:46 +0000

    Add follow function

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -12,7 +12,20 @@ export const fetchMoreData = async (resource, setResource) => {
                 : [...acc, cur]
             }, prevResource.results)
         }))
-    } catch(err){
+    } catch(err){}
+};
 
+export const followHelper = (profile, clickedProfile, following_id) => {
+    return profile.id === clickedProfile.id
+    ? {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
     }
+    : profile.is_owner
+    ? {
+        ...profile,
+        following_count: profile.following_count + 1
+    }
+    : profile;
 }

d293c1769682b8dab6c6acd293b0db2f12b3f62c
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-11 14:31:05 +0000

    Set up  jest test

---
+++ src/mocks/handlers.js
@@ -0,0 +1,16 @@
+const baseURL = "https://django-rest-kelz.herokuapp.com/"
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json({
+            "pk": 6,
+            "username": "Marina",
+            "email": "",
+            "first_name": "",
+            "last_name": "",
+            "profile_id": 6,
+            "profile_image": "https://res.cloudinary.com/dkolsfjkx/image/upload/v1/media/../default_profile_veem15"
+            })
+        );
+    }),
+];
